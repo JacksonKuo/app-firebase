41c0d883614062698dd8f369da7f1e87a7e7ac35
JacksonKuo <JacksonKuo@users.noreply.github.com>
2024-12-01 21:49:08 -0600

    Initial Commit

---
+++ app-firebase/app_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_firebaseApp.swift
+//  app-firebase
+//
+//  Created by JacksonKuo on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct app_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
